9964604ea48bf6d6bb33249321812ce70b0f9562
Matthew Camp <mattscamp@gmail.com>
2019-05-09 15:31:17 -0500

    Add new endpoint

--- src/endpoints/persons.ts
+++ src/endpoints/persons.ts
@@ -10,17 +10,22 @@ export default class Persons {
 
   async add(params: {
     name: string
-    ownerId?: number,
-    orgId?: number,
-    email?: string[],
-    phone?: string[],
-    visibleTo?: number,
-    addTime?: string,
+    ownerId?: number
+    orgId?: number
+    email?: string[]
+    phone?: string[]
+    visibleTo?: number
+    addTime?: string
   }) {
     const res = await HttpHandler.post(`${this.baseUrl}`, params, this.authToken)
     return res
   }
-  
+
+  async get(params: { id: number }) {
+    const res = await HttpHandler.get(`${this.baseUrl}/${params.id}`, {}, this.authToken)
+    return res
+  }
+
   async find(params: {
     term: string
     orgId?: number
